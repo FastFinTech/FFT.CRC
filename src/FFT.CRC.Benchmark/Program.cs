833ccc67310b843db0ff7d69db61fad3d857a537
True Goodwill <true@truegoodwill.com>
2021-01-09 20:27:56 -0500

    Added benchmarking and a performance optimization

---
+++ src/FFT.CRC.Benchmark/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace FFT.CRC.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PerformanceCounter>();
+        }
+    }
+
+    //[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    //[SimpleJob(RuntimeMoniker.NetCoreApp50)]
+    public class PerformanceCounter
+    {
+        byte[][] spans;
+
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            this.spans = new[] {
+                new byte[] {1,2,3},
+                new byte[] {4,5,6},
+                new byte[] {7,8,9},
+            };
+        }
+
+        [Benchmark]
+        public uint SpeedTest()
+        {
+            var builder = CRC32Builder.CreateInitialized();
+            for (var i = 0; i < 1024; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    builder.Add(this.spans[j]);
+                }
+            }
+            return builder.Value;
+        }
+    }
+}
--- src/FFT.CRC/CRC32Calculator.cs
+++ src/FFT.CRC/CRC32Calculator.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Buffers;
 using System.Runtime.CompilerServices;
 
 namespace FFT.CRC
@@ -17,37 +16,9 @@ namespace FFT.CRC
         /// </summary>
         public const uint SEED = 0xFFFFFFFF;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        private static uint[] table;
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        private static readonly uint[] table;
 
-        /// <summary>
-        /// Updates the running CRC32 calculation <paramref name="value"/>.
-        /// Call this method each time new data arrives, passing in the new <paramref name="bytes"/>.
-        /// You are responsible to maintain your own copy of <paramref name="value"/> until the
-        /// calculation process is complete.
-        /// </summary>
-        /// <param name="value">The running CRC32 calculation value which will be updated.</param>
-        /// <param name="bytes">The new data segment to "append" to the CRC32 calculation.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Add(ref uint value, ReadOnlySpan<byte> bytes)
-        {
-            foreach (var b in bytes)
-                value = (value >> 8) ^ table[(value ^ b) & 0xFF];
-        }
-
-        /// <summary>
-        /// After adding all the data you have received, call this method to obtain
-        /// the final CRC32 calculation result.
-        /// </summary>
-        /// <param name="value">The running CRC32 calculation value.</param>
-        /// <returns>The finalized CRC32 calculation result.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint Finalize(uint value)
-            => value ^ SEED;
-
-        [ModuleInitializer]
-        internal static void InitializeTable()
+        static CRC32Calculator()
         {
             const uint POLYNOMIAL = 0xEDB88320;
             table = new uint[256];
@@ -68,5 +39,42 @@ namespace FFT.CRC
                 table[i] = value;
             }
         }
+
+        /// <summary>
+        /// Updates the running CRC32 calculation <paramref name="value"/>.
+        /// Call this method each time new data arrives, passing in the new <paramref name="bytes"/>.
+        /// You are responsible to maintain your own copy of <paramref name="value"/> until the
+        /// calculation process is complete.
+        /// </summary>
+        /// <param name="value">The running CRC32 calculation value which will be updated.</param>
+        /// <param name="bytes">The new data segment to "append" to the CRC32 calculation.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void Add(ref uint value, ReadOnlySpan<byte> bytes)
+        {
+            // *************************************************************
+            //        SIMPLE BUT SLOWER VERSION
+            // *************************************************************
+            //foreach (var b in bytes)
+            //    value = (value >> 8) ^ table[(value ^ b) & 0xFF];
+            // *************************************************************
+
+            // Use the unsafe pointers to eliminate array bounds checking and creation of an enumerator
+            // From benchmark testing, we find it saves 2 microseconds for 1024 CRC calculations composed of 3 spans of 3 bytes each.
+            var length = bytes.Length;
+            fixed (uint* t = table)
+            fixed (byte* b = bytes)
+                for (var i = 0; i < length; i++)
+                    value = (value >> 8) ^ t[(value ^ b[i]) & 0xFF];
+        }
+
+        /// <summary>
+        /// After adding all the data you have received, call this method to obtain
+        /// the final CRC32 calculation result.
+        /// </summary>
+        /// <param name="value">The running CRC32 calculation value.</param>
+        /// <returns>The finalized CRC32 calculation result.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint Finalize(uint value)
+            => value ^ SEED;
     }
 }
